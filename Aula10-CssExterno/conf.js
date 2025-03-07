ab743c30556d97ef43f73c7a2c93c78419500b16
Arthur Cordova <arthurbscordova@gmail.com>
2025-03-06 21:55:56 -0300

    Codigos da aula de CSS

---
+++ Aula10-CssExterno/conf.js
@@ -0,0 +1,9 @@
+function amarelo() {
+    let texto = document.getElementById("box0");
+    texto.style.backgroundColor = "yellow";
+}
+
+function branco() {
+    let texto = document.getElementsByClassName("caixa2");
+    texto.style.backgroundColor = "yellow";
+}
